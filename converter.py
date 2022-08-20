af4d3ce4536ebcd4ca5afb75e258a1f511b31b3e
Quirin Möller <quirin.moeller@gmail.com>
2022-08-20 14:16:56 +0200

    generate gpx

--- converter.py
+++ converter.py
@@ -26,10 +26,34 @@ for match in trackpoint_pattern.finditer(text):
     pointtime = datetime.datetime(int(match.group('year')), int(match.group('month')), int(match.group('day')), int(match.group('hour')), int(match.group('minute')), int(match.group('second')));
     trackpoint = {
         'time': pointtime,
-        'latitude': match.group('latitude'),
-        'longitude': match.group('longitude'),
-        'altitude': match.group('altitude'),
-        'speed': match.group('speed')
+        'latitude': float(match.group('latitude').replace(',','.')),
+        'longitude': float(match.group('longitude').replace(',','.')),
+        'altitude': float(match.group('altitude').replace(',','.')),
+        'speed': float(match.group('speed').replace(',','.'))
     }
     data.append(trackpoint)
 
+gpx = gpxpy.gpx.GPX()
+
+# Create first track in our GPX:
+gpx_track = gpxpy.gpx.GPXTrack()
+gpx.tracks.append(gpx_track)
+
+# Create first segment in our GPX track:
+gpx_segment = gpxpy.gpx.GPXTrackSegment()
+gpx_track.segments.append(gpx_segment)
+
+# Create points:
+# gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1234, 5.1234, elevation=1234))
+# gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1235, 5.1235, elevation=1235))
+# gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1236, 5.1236, elevation=1236))
+for trackpoint in data:
+    gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(trackpoint['latitude'], trackpoint['longitude'], elevation=trackpoint['altitude'], time= trackpoint['time']))
+
+# You can add routes and waypoints, too...
+
+# print('Created GPX:', gpx.to_xml())
+
+f = open("3207498739.gpx", "w")
+f.write(gpx.to_xml())
+f.close()
