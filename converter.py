9a5ae5fca04e965723a67db5a3424126c9a1ab3c
Quirin Möller <quirin.moeller@gmail.com>
2022-08-20 13:44:08 +0200

    save trackpoints in list
    no idea if datetime works correctly

--- converter.py
+++ converter.py
@@ -1,5 +1,8 @@
 import re
+from time import time
 import pandas as pd
+import gpxpy.gpx
+import datetime
 
 # 1988/8/5  4:1:49
 # 48,221282
@@ -14,7 +17,19 @@ ifile = open("01.txt",'r')
 text = ifile.read()
 ifile.close()
 
-waypoint = re.compile(r'(?P<date>(?P<year>\d{4})\/(?P<month>\d{1,2})\/(?P<day>\d{1,2})  (?P<hour>\d{1,2}):(?P<minute>\d{1,2}):(?P<second>\d{1,2}))\n(?P<latitude>\d*,\d*)\n(?P<longitude>\d*,\d*)\n(?P<altitude>\d*,\d*)\n(?P<speed>\d*(,\d*)?)')
+trackpoint_pattern = re.compile(r'(?P<date>(?P<year>\d{4})\/(?P<month>\d{1,2})\/(?P<day>\d{1,2})  (?P<hour>\d{1,2}):(?P<minute>\d{1,2}):(?P<second>\d{1,2}))\n(?P<latitude>\d*,\d*)\n(?P<longitude>\d*,\d*)\n(?P<altitude>\d*,\d*)\n(?P<speed>\d*(,\d*)?)')
 
-for match in waypoint.finditer(text):
-    print(match.group('year'))
+data = []
+
+for match in trackpoint_pattern.finditer(text):
+    # print(match.group(0))
+    pointtime = datetime.datetime(int(match.group('year')), int(match.group('month')), int(match.group('day')), int(match.group('hour')), int(match.group('minute')), int(match.group('second')));
+    trackpoint = {
+        'time': pointtime,
+        'latitude': match.group('latitude'),
+        'longitude': match.group('longitude'),
+        'altitude': match.group('altitude'),
+        'speed': match.group('speed')
+    }
+    data.append(trackpoint)
+
